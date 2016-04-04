af643df3516753257c7265e37ff6ddf5a4dec446
EleonoraSmid <eleonora.smid@gmail.com>
2016-04-04 16:22:13 +0200

    Hi React

---
+++ dist/react/componentOne.js
@@ -0,0 +1,1 @@
+var person = React.createElement(Person, {name: window.isLoggedIn ? window.name : ''});
--- dist/react/main-react.js
+++ dist/react/main-react.js
@@ -1,1 +1,7 @@
-thins
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  React.createElement("h1", null, "Hello, world!"),
+  document.getElementById('content')
+);
